e836d94a8b8731f20eae59bb14c1049d2ebfa1b6
Duarte David <deltaduartedavid@gmail.com>
2018-01-24 18:28:37 +0100

    Use real github extention

--- src/register_apis.js
+++ src/register_apis.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = (Mozaik, configFile, config) => {
-    Mozaik.registerApi('github', require('../../mozaik-ext-github/client'))
+    Mozaik.registerApi('github', require('@mozaik/ext-github/client'))
     Mozaik.registerApi('nlbw', require('./nlbw/client'))
     Mozaik.registerApi('statuspage-io', require('./statuspage-io/client'))
 }
